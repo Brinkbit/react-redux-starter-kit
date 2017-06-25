32eba7c04abb87ea7cfb7cac84e7cc182e032bba
Justin Livi <justin@brinkbit.com>
2017-06-25 16:01:06 -0400

    Add brinkbit styling

--- src/store/createStore.js
+++ src/store/createStore.js
@@ -1,51 +1,51 @@
-import { applyMiddleware, compose, createStore as createReduxStore } from 'redux'
-import thunk from 'redux-thunk'
-import { browserHistory } from 'react-router'
-import makeRootReducer from './reducers'
-import { updateLocation } from './location'
+import { applyMiddleware, compose, createStore as createReduxStore } from 'redux';
+import thunk from 'redux-thunk';
+import { browserHistory } from 'react-router';
+import reducers from './reducers';
+import { updateLocation } from './location';
 
-const createStore = (initialState = {}) => {
-  // ======================================================
-  // Middleware Configuration
-  // ======================================================
-  const middleware = [thunk]
+const createStore = ( initialState = {}) => {
+    // ======================================================
+    // Middleware Configuration
+    // ======================================================
+    const middleware = [thunk];
 
-  // ======================================================
-  // Store Enhancers
-  // ======================================================
-  const enhancers = []
-  let composeEnhancers = compose
+    // ======================================================
+    // Store Enhancers
+    // ======================================================
+    const enhancers = [];
+    let composeEnhancers = compose;
 
-  if (__DEV__) {
-    if (typeof window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ === 'function') {
-      composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    if ( __DEV__ ) {
+        if ( typeof window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ === 'function' ) {
+            composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+        }
     }
-  }
 
-  // ======================================================
-  // Store Instantiation and HMR Setup
-  // ======================================================
-  const store = createReduxStore(
-    makeRootReducer(),
-    initialState,
-    composeEnhancers(
-      applyMiddleware(...middleware),
-      ...enhancers
-    )
-  )
-  store.asyncReducers = {}
+    // ======================================================
+    // Store Instantiation and HMR Setup
+    // ======================================================
+    const store = createReduxStore(
+        reducers(),
+        initialState,
+        composeEnhancers(
+            applyMiddleware( ...middleware ),
+            ...enhancers
+        )
+    );
+    store.asyncReducers = {};
 
-  // To unsubscribe, invoke `store.unsubscribeHistory()` anytime
-  store.unsubscribeHistory = browserHistory.listen(updateLocation(store))
+    // To unsubscribe, invoke `store.unsubscribeHistory()` anytime
+    store.unsubscribeHistory = browserHistory.listen( updateLocation( store ));
 
-  if (module.hot) {
-    module.hot.accept('./reducers', () => {
-      const reducers = require('./reducers').default
-      store.replaceReducer(reducers(store.asyncReducers))
-    })
-  }
+    if ( module.hot ) {
+        module.hot.accept( './reducers', () => {
+            const hotReducers = require( './reducers' ).default; // eslint-disable-line global-require
+            store.replaceReducer( hotReducers( store.asyncReducers ));
+        });
+    }
 
-  return store
-}
+    return store;
+};
 
-export default createStore
+export default createStore;
